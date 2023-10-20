e802eb1dd8a22aeeed36049bd9f37941b9da9c04
ChunEunyu <xhoneyx01@naver.com>
2023-10-20 14:30:45 +0900

    cover password

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express') // 익스프레스 모듈을 가져옴
 const app = express() //새로운 익스프레스 앱을 만듦
 const port = 5000 // 포트
 
-// mongodb+srv://whoyou48:jujunyu0718@cluster0.ndd9xwz.mongodb.net/?retryWrites=true&w=majority
+// mongodb+srv://whoyou48:<password>@cluster0.ndd9xwz.mongodb.net/?retryWrites=true&w=majority
 
 // hello world를 실행함
 app.get('/', (req, res) => {
